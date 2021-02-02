03c564853b3ccdd6315f542fec69b13058e61ecf
masafumi kato <xslash1105@gmail.com>
2021-02-03 02:14:19 +0900

    [add] contentのデザインを追加

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';s
+
+const styles = (theme: Theme) =>
+  createStyles({
+    paper: {
+      maxWidth: 936,
+      margin: 'auto',
+      overflow: 'hidden',
+    },
+    searchBar: {
+      borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+    },
+    searchInput: {
+      fontSize: theme.typography.fontSize,
+    },
+    block: {
+      display: 'block',
+    },
+    addUser: {
+      marginRight: theme.spacing(1),
+    },
+    contentWrapper: {
+      margin: '40px 16px',
+    },
+  });
+
+export interface ContentProps extends WithStyles<typeof styles> {}
+
+function Content(props: ContentProps) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.paper}>
+      <AppBar className={classes.searchBar} position="static" color="default" elevation={0}>
+        <Toolbar>
+          <Grid container spacing={2} alignItems="center">
+            現在のオンラインユーザー数
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.contentWrapper}>
+        <Typography color="textSecondary" variant="h4" component="p" align="center">
+          10 人
+        </Typography>
+      </div>
+    </Paper>
+  );
+}
+
+export default withStyles(styles)(Content);
