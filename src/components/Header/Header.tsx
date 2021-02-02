2fa68f89b31a72a27c97feb2d6fda041f0b69b07
masafumi kato <xslash1105@gmail.com>
2021-02-03 02:14:03 +0900

    [add] headerのデザインを追加

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+
+const lightColor = 'rgba(255, 255, 255, 0.7)';
+
+const themeColor = '#EB6100';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    header: {
+      backgroundColor: themeColor,
+    },
+    secondaryBar: {
+      backgroundColor: themeColor,
+      zIndex: 0,
+    },
+    menuButton: {
+      marginLeft: -theme.spacing(1),
+    },
+    iconButtonAvatar: {
+      padding: 4,
+    },
+    link: {
+      textDecoration: 'none',
+      color: lightColor,
+      '&:hover': {
+        color: theme.palette.common.white,
+      },
+    },
+    button: {
+      borderColor: lightColor,
+    },
+  });
+
+interface HeaderProps extends WithStyles<typeof styles> {
+  onExitHandler: () => void;
+}
+
+function Header(props: HeaderProps) {
+  const { classes, onExitHandler } = props;
+
+  return (
+    <React.Fragment>
+      <AppBar color="primary" position="sticky" elevation={0} className={classes.header}>
+        <Toolbar />
+      </AppBar>
+      <AppBar
+        component="div"
+        className={classes.secondaryBar}
+        color="primary"
+        position="static"
+        elevation={0}
+      >
+        <Toolbar>
+          <Grid container alignItems="center" spacing={1}>
+            <Grid item xs>
+              <Typography color="inherit" variant="h5" component="h1">
+                「Mokux2」にようこそ！今日もがんばっていきましょう！
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button className={classes.button} variant="outlined" color="inherit" size="small" onClick={onExitHandler}>
+                退出する
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <AppBar
+        component="div"
+        className={classes.secondaryBar}
+        color="primary"
+        position="static"
+        elevation={0}
+      >
+        <Toolbar>
+          <Grid container alignItems="center" spacing={1}>
+            <Grid item xs>
+              <Typography color="inherit" >
+                「Mokux2」はオンライン上で不特定多数と「もくもく会」をする場所を提供し、他の頑張っている人を見てモチベーションを生むきっかけを作ります。<br />
+              </Typography>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+}
+
+export default withStyles(styles)(Header);
