9e2ce1bb26b054a490588136ae84bc2e971ce190
masafumi kato <xslash1105@gmail.com>
2021-02-03 02:15:26 +0900

    [update] roomの処理に新しく追加したデザインを反映

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -1,36 +1,214 @@
 import React from 'react';
+import {
+  createMuiTheme,
+  createStyles,
+  ThemeProvider,
+  withStyles,
+  WithStyles,
+} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Hidden from '@material-ui/core/Hidden';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Navigator from '../../components/Navigator/Navigator';
+import Content from '../../components/Content/Content';
+import Header from '../../components/Header/Header';
 
-const Room: React.FC = () => {
+function Copyright() {
   return (
-    <div>
-      <div className="leftWrapper">
-        <header className="roomHeader">
-          Mokux2
-        </header>
-        <div className="roomContent">
-          入場しました！
-          現在〇〇人が作業中です。
-        </div>
-      </div>
-      <div className="rightWrapper">
-        <h3>オンライン</h3>
-        <ul>
-          <li className="onlineUserWrapper">
-            <span className="onlineIcon">○</span>
-            <span className="onlineUser">Aさん</span>
-          </li>
-          <li className="onlineUserWrapper">
-            <span className="onlineIcon">○</span>
-            <span className="onlineUser">Aさん</span>
-          </li>
-          <li className="onlineUserWrapper">
-            <span className="onlineIcon">○</span>
-            <span className="onlineUser">Aさん</span>
-          </li>
-        </ul>
-      </div>
-    </div>
-  )
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Mokux2
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Room;
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#63ccff',
+      main: '#009be5',
+      dark: '#006db3',
+    },
+  },
+  typography: {
+    h5: {
+      fontWeight: 500,
+      fontSize: 26,
+      letterSpacing: 0.5,
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  props: {
+    MuiTab: {
+      disableRipple: true,
+    },
+  },
+  mixins: {
+    toolbar: {
+      minHeight: 48,
+    },
+  },
+});
+
+theme = {
+  ...theme,
+  overrides: {
+    MuiDrawer: {
+      paper: {
+        backgroundColor: '#18202c',
+      },
+    },
+    MuiButton: {
+      label: {
+        textTransform: 'none',
+      },
+      contained: {
+        boxShadow: 'none',
+        '&:active': {
+          boxShadow: 'none',
+        },
+      },
+    },
+    MuiTabs: {
+      root: {
+        marginLeft: theme.spacing(1),
+      },
+      indicator: {
+        height: 3,
+        borderTopLeftRadius: 3,
+        borderTopRightRadius: 3,
+        backgroundColor: theme.palette.common.white,
+      },
+    },
+    MuiTab: {
+      root: {
+        textTransform: 'none',
+        margin: '0 16px',
+        minWidth: 0,
+        padding: 0,
+        [theme.breakpoints.up('md')]: {
+          padding: 0,
+          minWidth: 0,
+        },
+      },
+    },
+    MuiIconButton: {
+      root: {
+        padding: theme.spacing(1),
+      },
+    },
+    MuiTooltip: {
+      tooltip: {
+        borderRadius: 4,
+      },
+    },
+    MuiDivider: {
+      root: {
+        backgroundColor: '#404854',
+      },
+    },
+    MuiListItemText: {
+      primary: {
+        fontWeight: theme.typography.fontWeightMedium,
+      },
+    },
+    MuiListItemIcon: {
+      root: {
+        color: 'inherit',
+        marginRight: 0,
+        '& svg': {
+          fontSize: 20,
+        },
+      },
+    },
+    MuiAvatar: {
+      root: {
+        width: 32,
+        height: 32,
+      },
+    },
+  },
+};
+
+const drawerWidth = 256;
+
+const styles = createStyles({
+  root: {
+    display: 'flex',
+    minHeight: '100vh',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  app: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  main: {
+    flex: 1,
+    padding: theme.spacing(6, 4),
+    background: '#eaeff1',
+  },
+  footer: {
+    padding: theme.spacing(2),
+    background: '#eaeff1',
+  },
+});
+
+export interface RoomProps extends WithStyles<typeof styles> {}
+
+const Room: React.FC<RoomProps> = (props) => {
+  const { classes } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const onExitHandler = () => {
+    console.log('exited');
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <nav className={classes.drawer}>
+          <Hidden smUp implementation="js">
+            <Navigator
+              PaperProps={{ style: { width: drawerWidth } }}
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+            />
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Navigator PaperProps={{ style: { width: drawerWidth } }} />
+          </Hidden>
+        </nav>
+        <div className={classes.app}>
+          <Header onExitHandler={onExitHandler} />
+          <main className={classes.main}>
+            <Content />
+          </main>
+          <footer className={classes.footer}>
+            <Copyright />
+          </footer>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default withStyles(styles)(Room);
