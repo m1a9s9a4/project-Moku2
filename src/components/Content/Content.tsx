8b8b6e2acc349eac38f5e99564cc84fa4e80210d
masafumi kato <xslash1105@gmail.com>
2021-02-03 22:57:13 +0900

    [update] ログイン後のメインコンテンツを追加

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -4,7 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';s
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
 
 const styles = (theme: Theme) =>
   createStyles({
