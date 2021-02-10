040aa03b57822e42d2e438e678f564f497558f59
masafumi kato <xslash1105@gmail.com>
2021-02-10 20:55:46 +0900

    [update] entryページからauthに認証処理を移動

--- src/pages/Entry/Entry.tsx
+++ src/pages/Entry/Entry.tsx
@@ -1,37 +1,23 @@
 import React, {useState} from 'react';
-import Firebase from "firebase/app";
 import EntryInput from '../../components/EntryInput/EntryInput';
+import {useAuth} from "../../contexts/AuthContext";
+import {useHistory} from 'react-router-dom';
 
 interface Props {
-  auth: Firebase.auth.Auth;
-  database: Firebase.database.Database;
-  setOnline: (bool: boolean) => void;
 }
 
 const Entry: React.FC<Props> = (props) => {
-  const {auth, database, setOnline} = props;
-  const [error, setError] = useState('');
-  const onEntryHandler = async () => {
-    const provider = new Firebase.auth.GoogleAuthProvider();
-    const user = await auth.signInWithPopup(provider)
-      .then(({user}) => {
-        return user;
-      })
-      .catch((err) => {
-        setError('ログイン中にエラーが発生しました');
-        console.error(err);
-        return null;
-      })
+  const {login} = useAuth();
+  const history = useHistory();
 
-    if (user) {
-      await database.ref('/online/' + user.uid).set({
-        last_change: Firebase.database.ServerValue.TIMESTAMP,
-        username: user.uid.toLowerCase().slice(0, 5),
-      })
-        .then(() => {
-          setOnline(true);
-          window.location.href = '/';
-        });
+  const onEntryHandler = async () => {
+    try {
+      console.log('log in started...')
+      await login();
+      history.push('/');
+      console.log('log in finished...')
+    } catch (e) {
+      console.error('failed to login');
     }
   }
   return (
