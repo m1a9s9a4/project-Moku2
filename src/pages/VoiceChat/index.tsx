6b268220f42a168eb18c4a5bdd0b228c6268dd3a
masafumi kato <xslash1105@gmail.com>
2021-02-27 20:47:56 +0900

    [add] 音声で話せるページを作成

---
+++ src/pages/VoiceChat/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import VideoChatContext from '../../contexts/VoiceChatContext';
+
+const Main: React.FC = () => {
+  return (
+    <div>
+      <VideoChatContext />
+    </div>
+  )
+}
+
+export default Main;
